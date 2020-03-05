f2eb247f4d0b66f10bfbfcc40b2d1bce6195ab9c
AdrienBarens <abarens@csumb.edu>
2020-03-04 19:20:30 -0800

    adding API

--- js/app.js
+++ js/app.js
@@ -1,23 +1,46 @@
 var entry = $("#entry");
 
+$("#btn").on('click', search);
 
-$("#btn").on('click',search);
+//$("#isbn").on('submit',search);
 
 function validEntry() {
     var input = Number(entry.val());
-    console.log(entry.val());
-    console.log(input);
     if (isNaN(input)) {
         return false
     }
     return true;
 }
 
+//0451526538
+
 function search() {
     entry.css("border-style",'');
     entry.css("border-color",'');
     if (validEntry()) {
-        console.log("OK");
+        var link = "ISBN:" + entry.val();
+        console.log(link);
+        $.ajax({
+            method: 'GET',
+            url: 'https://openlibrary.org/api/books',
+            dataType: 'json',
+            data: {
+                bibkeys: link,
+                format: 'json',
+                jscmd: "data"
+            },
+            success: function (data) {
+                $("#display").html('');
+                var result = data[link];
+                console.log(result);
+                $("#display").append('<div id="cover" class="col-md-3"><img alt="cover" src="' + result.cover.large + '"></div>');
+                $("#display").append('<div id="info" class="col-md-4">Title : ' + result.title + '<br>' +
+                    'Author: ' + result.authors[0].name + '<br>' +
+                    'Publish: ' + result.publish_date + '<br>' +
+                    'Publisher: ' + "undefined" + '<br>' + link + '<br>' +
+                    'Pages: ' + result.number_of_pages +'</div>');
+            }
+        })
     } else {
         entry.css("border-style",'solid');
         entry.css("border-color",'red');
